fce3fbefffecaa2c2da3c8e0e8c7872df2c455fc
JinnFighter <chernayamolnia@yandex.ru>
2019-03-17 13:50:59 +0400

    Added Main Menu, loads Color Picker mode now

---
+++ Assets/Scripts/MainSettings.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainSettings : MonoBehaviour
+{
+    private bool isFullscreen;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isFullscreen = Screen.fullScreen;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void FullscreenToggle()
+    {
+        isFullscreen = !isFullscreen;
+        Screen.fullScreen = isFullscreen;
+    }
+}
