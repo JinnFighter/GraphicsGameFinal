d0b6b5b69f59d05dd2d746d43aa335ca25e2c115
JinnFighter <chernayamolnia@yandex.ru>
2019-03-19 16:53:41 +0400

    Added Brezenheim Scene

---
+++ Assets/Scripts/Brezenheim/BrezenheimGameController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrezenheimGameController : MonoBehaviour
+{
+    [SerializeField] public GridPixel originalPixel;
+    public const int gridRows = 10;
+    public const int gridCols = 10;
+    public const float offsetX = 0.5f;
+    public const float offsetY = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 startPos = originalPixel.transform.position;
+
+        
+        /* for(int i=0;i<numbers.Length;i++)
+         {
+             Debug.Log(numbers[i] + " ");
+         }*/
+        for (int i = 0; i < gridRows; i++)
+        {
+            for (int j = 0; j < gridCols; j++)
+            {
+                GridPixel pixel;
+                if (i == 0 && j == 0)
+                {
+                    pixel = originalPixel;
+                }
+                else
+                {
+                    pixel = Instantiate(originalPixel) as GridPixel;
+                }
+
+                
+
+                //int id = Random.Range(0, images.Length);
+                float posX = (offsetX * j) + startPos.x;
+                float posY = -(offsetY * i) + startPos.y;
+                pixel.transform.position = new Vector3(posX, posY, startPos.z);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Brezenheim/GridPixel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridPixel : MonoBehaviour
+{
+    [SerializeField] public GameObject pixel_empty;
+    private bool painted;
+    // Start is called before the first frame update
+    void Start()
+    {
+        painted = false;
+        pixel_empty.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void OnMouseDown()
+    {
+
+    }
+}
