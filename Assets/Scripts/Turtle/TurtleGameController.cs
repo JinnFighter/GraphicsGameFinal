fd43871dfb628b93a71a4227bdc1b68ba392c36a
JinnFighter <chernayamolnia@yandex.ru>
2019-04-02 14:22:16 +0400

    Created Turtle scene

---
+++ Assets/Scripts/Turtle/TurtleGameController.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurtleGameController : MonoBehaviour
+{
+    [SerializeField] public TurtleGridPixelScript originalPixel;
+    private TurtleGridPixelScript[,] grid;
+    public const int gridRows = 10;
+    public const int gridCols = 10;
+    private float offsetX;
+    private float offsetY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        grid = new TurtleGridPixelScript[gridRows, gridCols];
+        Vector3 startPos = originalPixel.transform.position;
+
+        offsetX = originalPixel.GetComponent<SpriteRenderer>().bounds.size.x;
+        offsetY = originalPixel.GetComponent<SpriteRenderer>().bounds.size.y;
+        /* for(int i=0;i<numbers.Length;i++)
+         {
+             Debug.Log(numbers[i] + " ");
+         }*/
+        for (int i = 0; i < gridRows; i++)
+        {
+            for (int j = 0; j < gridCols; j++)
+            {
+                TurtleGridPixelScript pixel;
+
+                if (i == 0 && j == 0)
+                {
+                    pixel = originalPixel;
+                    //grid[i,j] = originalPixel;
+                }
+                else
+                {
+                    //grid[i,j] = Instantiate(originalPixel) as GridPixelScript;
+                    pixel = Instantiate(originalPixel) as TurtleGridPixelScript;
+                    float posX = (offsetX * j) + startPos.x;
+                    float posY = -(offsetY * i) + startPos.y;
+                    pixel.transform.position = new Vector3(posX, posY, startPos.z);
+                }
+                //int id = Random.Range(0, images.Length);
+
+                //pixel.setPixelState(true);
+                //grid[i,j].transform.position = new Vector3(posX, posY, startPos.z);
+
+                //pixel.transform.SetParent(this.transform.Find("Canvas").transform);
+                //pixel.setPixelState(true);
+                grid[i, j] = pixel;
+
+                //Debug.Log(grid[i, j].GetHashCode().ToString());
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Turtle/TurtleGridPixelScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurtleGridPixelScript : MonoBehaviour
+{
+    [SerializeField] public GameObject pixel_empty;
+    [SerializeField] public GameObject gameController;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void setPixelState(bool state)
+    {
+        pixel_empty.SetActive(!state);
+    }
+    public void OnMouseDown()
+    {
+        setPixelState(true);
+    }
+}
