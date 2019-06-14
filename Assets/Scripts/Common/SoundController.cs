181f9cae13a96ae5e48c94b3c7e27bbf716df411
JinnFighter <chernayamolnia@yandex.ru>
2019-06-14 23:28:09 +0400

    Added SoundController Class for handling sounds and playing music in game. Also added some free chiptunes and 8-bit sounds to the game(menu only for now)

---
+++ Assets/Scripts/Common/SoundController.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour
+{
+    [SerializeField]private AudioSource source;
+    [SerializeField]private AudioClip correctAnswerClip;
+    [SerializeField]private AudioClip wrongAnswerClip;
+    [SerializeField]private AudioClip gameOverClip;
+    [SerializeField]private AudioClip buttonClickClip;
+    [SerializeField]private AudioClip selectProfileClip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Messenger<int>.AddListener(GameEvents.ACTION_RIGHT_ANSWER, PlayCorrectAnswerClip);
+        Messenger.AddListener(GameEvents.ACTION_WRONG_ANSWER, PlayWrongAnswerClip);
+        Messenger.AddListener(GameEvents.GAME_OVER, PlayGameOverClip);
+    }
+    void OnDestroy()
+    {
+        Messenger<int>.RemoveListener(GameEvents.ACTION_RIGHT_ANSWER, PlayCorrectAnswerClip);
+        Messenger.RemoveListener(GameEvents.ACTION_WRONG_ANSWER, PlayWrongAnswerClip);
+        Messenger.RemoveListener(GameEvents.GAME_OVER, PlayGameOverClip);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void PlayCorrectAnswerClip(int a)
+    {
+        if(source.clip!=correctAnswerClip)
+        {
+            source.clip = correctAnswerClip;
+        }
+        source.Play();
+    }
+    public void PlayWrongAnswerClip()
+    {
+        if(source.clip!=wrongAnswerClip)
+        {
+            source.clip = wrongAnswerClip;
+        }
+        source.Play();
+    }
+    public void PlayGameOverClip()
+    {
+        if (source.clip != gameOverClip)
+        {
+            source.clip = gameOverClip;
+        }
+        source.Play();
+    }
+    public void PlayButtonClickClip()
+    {
+        if (source.clip != buttonClickClip)
+        {
+            source.clip = buttonClickClip;
+        }
+        source.Play();
+    }
+    public void PlaySelectProfileClip()
+    {
+        if (source.clip != selectProfileClip)
+        {
+            source.clip = selectProfileClip;
+        }
+        source.Play();
+    }
+
+}
