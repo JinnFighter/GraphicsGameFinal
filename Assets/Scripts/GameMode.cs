f0908fa6fcb0e21b452fd57e3a4d35da77cab6ce
JinnFighter <chernayamolnia@yandex.ru>
2020-04-19 18:29:00 +0400

    add: GameMode abstract class

---
+++ Assets/Scripts/GameMode.cs
@@ -0,0 +1,37 @@
+﻿public abstract class GameMode
+{
+    private readonly GameplayTimer _timer;
+    private bool _gameActive;
+    private bool _gameStarted;
+    private int _difficulty;
+
+    public GameMode(GameplayTimer timer, int difficulty)
+    {
+        _gameStarted = false;
+        _gameActive = false;
+        _timer = timer;
+        _difficulty = difficulty;
+    }
+
+    public abstract void CheckAction(GridPixelScript invoker);
+
+    public virtual void Pause()
+    {
+        if (_gameStarted)
+        {
+            _gameActive = false;
+            _timer.PauseTimer();
+        }
+    }
+
+    public virtual void Continue()
+    {
+        if (_gameStarted)
+        {
+            _gameActive = true;
+            _timer.ResumeTimer();
+        }
+    }
+
+    public abstract void Restart();
+}
