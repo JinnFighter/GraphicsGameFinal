dbf0872e0b9b89b743d32da859c96651410a9ad0
JinnFighter <chernayamolnia@yandex.ru>
2020-04-19 18:40:25 +0400

    add: Brezenheim game mode

---
+++ Assets/Scripts/Brezenheim/BrezenheimGameMode.cs
@@ -0,0 +1,11 @@
+﻿public class BrezenheimGameMode : GameMode
+{
+    public BrezenheimGameMode(GameplayTimer timer, int difficulty) : base(timer, difficulty)
+    {
+
+    }
+
+    public override void CheckAction(GridPixelScript invoker) => throw new System.NotImplementedException();
+
+    public override void Restart() => throw new System.NotImplementedException();
+}
