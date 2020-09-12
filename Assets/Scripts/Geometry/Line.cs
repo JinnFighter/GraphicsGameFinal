abb09b045c3eb6b2240bba5953b303e2f4530ecd
Jinn <chernayamolnia@yandex.ru>
2020-09-12 17:18:33 +0400

    added Line and Position classes

---
+++ Assets/Scripts/Geometry/Line.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Line
+{
+    private Position _start;
+    private Position _end;
+
+    public Line(Position start, Position end)
+    {
+        _start = start;
+        _end = end;
+    }
+
+    public double GetLength() 
+    {
+        var xdif = _end.X - _start.X;
+        var ydif = _end.Y - _start.Y;
+        return Math.Sqrt(xdif * xdif + ydif * ydif); 
+    }
+}
---
+++ Assets/Scripts/Geometry/Position.cs
@@ -0,0 +1,14 @@
+﻿public class Position
+{
+    public double X { get; set; }
+
+    public double Y { get; set; }
+
+    public Position(double x, double y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public bool Equals(Position other) => X == other.X && Y == other.Y;
+}
