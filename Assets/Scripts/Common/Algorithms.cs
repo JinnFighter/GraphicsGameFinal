4b1f516cb5a5f3c5622a236dbced9a5ba7c33173
Jinn <chernayamolnia@yandex.ru>
2020-09-12 15:48:45 +0400

    removed unused Algorithms.Brezenheim4Circle method

--- Assets/Scripts/Common/Algorithms.cs
+++ Assets/Scripts/Common/Algorithms.cs
@@ -149,39 +149,6 @@ public class Algorithms : MonoBehaviour
         }
     }
 
-    public void Brezenheim4Circle(int Xc, int Yc, int r)
-    {
-        var xc = Yc;
-        var yc = Xc;
-        int x, y, d;
-        x = 0;
-        y = r;
-        d = 3 - 2 * y;
-        var field = GetComponent<GameField>();
-        while (x <= y)
-        {
-            field.grid[x + xc, y + yc].setPixelState(true);
-            field.grid[x + xc, -y + yc].setPixelState(true);
-            field.grid[-x + xc, -y + yc].setPixelState(true);
-            field.grid[-x + xc, y + yc].setPixelState(true);
-            field.grid[y + xc, x + yc].setPixelState(true);
-            field.grid[y + xc, -x + yc].setPixelState(true);
-            field.grid[-y + xc, -x + yc].setPixelState(true);
-            field.grid[-y + xc, x + yc].setPixelState(true);
-
-            if (d < 0)
-            {
-                d = d + 4 * x + 6;
-            }
-            else
-            {
-                d = d + 4 * (x - y) + 10;
-                y--;
-            }
-            x++;
-        }
-    }
-
     public static void DrawBezier(GameField field, List<Pixel> curvePoints)
     {
         double t, sx, sy, oldx, oldy, ax, ay, tau;
