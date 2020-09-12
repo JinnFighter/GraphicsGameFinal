31a6f9684b246cf45ebf9f2830725726aca3be17
Jinn <chernayamolnia@yandex.ru>
2020-09-12 21:12:17 +0400

    MultipleBrezenheimGameMode class now uses ILineGenerator classes

--- Assets/Scripts/Brezenheim/MultipleBrezenheimGameMode.cs
+++ Assets/Scripts/Brezenheim/MultipleBrezenheimGameMode.cs
@@ -3,6 +3,7 @@ using UnityEngine.UI;
 
 public class MultipleBrezenheimGameMode : GameMode
 {
+    private ILineGenerator _lineGenerator;
     private Line[] lines;
     private int linesQuantity;
     private List<Position>[] linePoints;
@@ -54,8 +55,14 @@ public class MultipleBrezenheimGameMode : GameMode
             linePoints[i] = new List<Position>();
             ds[i] = new List<int>();
         }
-
-        GeneratePolygon();
+        _lineGenerator = new PolygonLineGenerator(minLineLength, maxLineLength);
+        var lns = _lineGenerator.Generate(linesQuantity);
+        for(var i = 0; i < linesQuantity; i++)
+        {
+            lines[i] = lns[i];
+            linePoints[i] = Algorithms.GetBrezenheimLineData(lines[i], out var dds);
+            ds[i] = dds;
+        }    
 
         last_point = linePoints[0][linePoints[0].Count - 1];
         _gameField.grid[(int)lines[0].GetStart().X, (int)lines[0].GetStart().Y].setPixelState(true);
@@ -138,104 +145,18 @@ public class MultipleBrezenheimGameMode : GameMode
         cur_line = 0;
         iteration = 0;
 
-        GeneratePolygon();
+        _lineGenerator = new PolygonLineGenerator(minLineLength, maxLineLength);
+        var lns = _lineGenerator.Generate(linesQuantity);
+        for (var i = 0; i < linesQuantity; i++)
+        {
+            lines[i] = lns[i];
+            linePoints[i] = Algorithms.GetBrezenheimLineData(lines[i], out var dds);
+            ds[i] = dds;
+        }
         last_point = linePoints[0][linePoints[0].Count - 1];
         _gameField.grid[(int)lines[0].GetStart().X, (int)lines[0].GetStart().Y].setPixelState(true);
         _gameField.grid[(int)lines[0].GetEnd().X, (int)lines[0].GetEnd().Y].setPixelState(true);
         eventReactor.OnRestart();
         Messenger.Broadcast(GameEvents.START_GAME);
-    }
-
-    public void GeneratePolygon()
-    {
-        var lPoints = new List<Position>[linesQuantity];
-        for (var i = 0; i < linesQuantity; i++)
-        {
-            lPoints[i] = new List<Position>();
-            int x0;
-            int y0;
-
-            int x1;
-            int y1;
-
-            if (i == 0)
-            {
-                x0 = UnityEngine.Random.Range(0, 9);
-                y0 = UnityEngine.Random.Range(0, 9);
-
-                x1 = UnityEngine.Random.Range(0, 9);
-                y1 = UnityEngine.Random.Range(0, 9);
-
-                var line = new Line(new Position(x0, y0), new Position(x1, y1));
-                var lineLength = line.GetLength();
-                while (lineLength > maxLineLength
-                    || lineLength < minLineLength)
-                {
-                    x1 = UnityEngine.Random.Range(0, 9);
-                    y1 = UnityEngine.Random.Range(0, 9);
-                    var end = line.GetEnd();
-                    end.X = x1;
-                    end.Y = y1;
-                    lineLength = line.GetLength();
-                }
-            }
-            else
-            {
-                x0 = (int)lines[i - 1].GetEnd().Y;
-                y0 = (int)lines[i - 1].GetEnd().X;
-                if (i == linesQuantity - 1)
-                {
-                    x1 = (int)lines[0].GetStart().Y;
-                    y1 = (int)lines[0].GetStart().X;
-                }
-                else
-                {
-                    while (true)
-                    {
-                        x1 = UnityEngine.Random.Range(0, 9);
-                        y1 = UnityEngine.Random.Range(0, 9);
-                        var line = new Line(new Position(x0, y0), new Position(x1, y1));
-                        var lineLength = line.GetLength();
-                        if (lineLength > maxLineLength
-                        || lineLength <= minLineLength - 1)
-                        {
-                            var end = line.GetEnd();
-                            end.X = x1;
-                            end.Y = y1;
-                            lineLength = line.GetLength();
-                            continue;
-                        }
-
-                        bool check = false;
-                        for (var j = 0; j < i - 1; j++)
-                        {
-                            if (HasSegmentsIntersection(lines[j].GetStart(), lPoints[j][lPoints[j].Count - 2],
-                             new Position(y0, x0), new Position(y1, x1)));
-                            {
-                                break;
-                            }
-                        }
-
-                        if (check)
-                            continue;
-                        else
-                            break;
-                    }
-                }
-            }
-
-            lines[i] = new Line(new Position(y0, x0), new Position(y1, x1));
-            lPoints[i] = Algorithms.GetBrezenheimLineData(new Line(new Position(x0, y0), new Position(x1, y1)), out ds[i]);
-            linePoints[i] = Algorithms.GetBrezenheimLineData(new Line(new Position(x0, y0), new Position(x1, y1)), out ds[i]);
-        }
-    }
-
-    private bool HasSegmentsIntersection(Position a, Position b, Position c, Position d)
-    {
-        int v1 = (int)((d.X - c.X) * (a.Y - c.Y) - (d.Y - c.Y) * (a.X - c.X));
-        int v2 = (int)((d.X - c.X) * (b.Y - c.Y) - (d.Y - c.Y) * (b.X - c.X));
-        int v3 = (int)((b.X - a.X) * (c.Y - a.Y) - (b.Y - a.Y) * (c.X - a.X));
-        int v4 = (int)((b.X - a.X) * (d.Y - a.Y) - (b.Y - a.Y) * (d.X - a.X));
-        return (v1 * v2 < 0) && (v3 * v4 < 0);
     }
 }
