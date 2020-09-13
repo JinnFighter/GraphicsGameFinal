d5f98a604bbf66d1192e779b963739bcb7354e46
Jinn <chernayamolnia@yandex.ru>
2020-09-13 12:50:27 +0400

    added LinesModeData class

---
+++ Assets/Scripts/LinesModeData.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+
+public class LinesModeData
+{
+    private List<Position> _linePoints;
+    private int _currentPoint;
+
+    public LinesModeData()
+    {
+        _linePoints = new List<Position>();
+        _currentPoint = 0;
+    }
+
+    public Position GetCurrentPoint() => _linePoints[_currentPoint];
+
+    public Position GetPoint(int index) => _linePoints[index];
+
+    public int GetPointsCount() => _linePoints.Count;
+}
