16de00a36e066ca160c1ca0b74629f250a19d833
Jinn <chernayamolnia@yandex.ru>
2020-09-13 14:18:20 +0400

    moved generation of bezier curve to separate class

--- Assets/Scripts/Bezier/BezierGameMode.cs
+++ Assets/Scripts/Bezier/BezierGameMode.cs
@@ -1,16 +1,15 @@
-﻿using System.Collections.Generic;
-
-public class BezierGameMode : GameMode
+﻿public class BezierGameMode : GameMode
 {
     private LinesModeData _linesData;
     private ILineDataGenerator _generator;
+    private ILinePointsGenerator _pointsGenerator;
     private GameField _gameField;
 
     public BezierGameMode(GameplayTimer timer, int difficulty, GameField field) : base(difficulty)
     {
         _gameField = field;
         _linesData = new LinesModeData();
-
+        _pointsGenerator = new BezierLinePointsGenerator();
         Generate();
 
         eventReactor = new DefaultReactor(timer, difficulty);
@@ -31,14 +30,10 @@ public class BezierGameMode : GameMode
                 Messenger<int>.Broadcast(GameEvents.ACTION_RIGHT_ANSWER, 100);
                 _linesData.NextPoint();
                 if (_linesData.IsCurrentLast())
-                {
                     eventReactor.OnGameOver();
-                }
             }
             else
-            {
                 Messenger.Broadcast(GameEvents.ACTION_WRONG_ANSWER);
-            }
         }
     }
 
@@ -52,66 +47,6 @@ public class BezierGameMode : GameMode
 
         eventReactor.OnRestart();
         Messenger.Broadcast(GameEvents.START_GAME);
-    }
-
-    private void DrawBezier(List<Position> curvePoints)
-    {
-        var points = new List<Position>();
-        double t, sx, sy, oldx, oldy, ax, ay, tau;
-        oldx = curvePoints[0].X;
-        oldy = curvePoints[0].Y;
-        var counter = curvePoints.Count;
-
-        for (t = 0; t <= 0.5; t += 0.005)
-        {
-            sx = curvePoints[0].X;
-            sy = curvePoints[0].Y;
-            ax = 1.0;
-            ay = 1.0;
-            tau = 1.0;
-            for (int i = 1; i < counter; i++)//counter;
-            {
-                tau *= (1 - t);
-                ax = ax * t * (counter - i) / (i * (1 - t));
-                ay = ay * t * (counter - i) / (i * (1 - t));
-                sx += ax * curvePoints[i].X;
-                sy += ay * curvePoints[i].Y;
-            }
-            sx *= tau;
-            sy *= tau;
-
-            points.AddRange(Algorithms.GetBrezenheimLineData(new Line(new Position(oldx, oldy), new Position(sx, sy)), out _));
-
-            oldx = sx;
-            oldy = sy;
-        }
-        oldx = curvePoints[counter - 1].X;
-        oldy = curvePoints[counter - 1].Y;
-        for (t = 1.0; t >= 0.5; t -= 0.005)
-        {
-            sx = curvePoints[counter - 1].X;
-            sy = curvePoints[counter - 1].Y;
-            ax = 1.0;
-            ay = 1.0;
-            tau = 1.0;
-            for (var i = counter - 2; i >= 0; i--)
-            {
-                tau *= t;
-                ax = ax * (1 - t) * (i + 1) / (t * (counter - 1 - i));
-                ay = ay * (1 - t) * (i + 1) / (t * (counter - 1 - i));
-                sx += ax * curvePoints[i].X;
-                sy += ay * curvePoints[i].Y;
-            }
-            sx *= tau;
-            sy *= tau;
-
-            points.AddRange(Algorithms.GetBrezenheimLineData(new Line(new Position(oldx, oldy), new Position(sx, sy)), out _));
-
-            oldx = sx;
-            oldy = sy;
-        }
-
-        _gameField.Draw(points);
     }
 
     private void Generate()
@@ -140,11 +75,8 @@ public class BezierGameMode : GameMode
 
         _generator = new BezierLineDataGenerator(pointsCount);
         _linesData = _generator.GenerateData(minLength, maxLength);
-        var curvePoints = new List<Position>();
 
-        for(var i = 0; i < _linesData.GetPointsCount(); i++)
-            curvePoints.Add(_linesData.GetPoint(i));
-
-        DrawBezier(curvePoints);
+        var points = _pointsGenerator.Generate(_linesData);
+        _gameField.Draw(points);
     }
 }
---
+++ Assets/Scripts/Brezenheim/ILinePointsGenerator.cs
@@ -0,0 +1,6 @@
+﻿using System.Collections.Generic;
+
+public interface ILinePointsGenerator
+{
+    List<Position> Generate(LinesModeData linesData);
+}
