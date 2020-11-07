3295480fd036a5fee4f6dce0aaed37260daa1bc7
Jinn <chernayamolnia@yandex.ru>
2020-11-07 19:16:36 +0400

    added TimerComponent class

---
+++ Assets/Scripts/Common/GameplayTimer/TimerComponent.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class TimerComponent : MonoBehaviour
+{
+    public float CurrentTime { get; private set; }
+    public bool IsCounting { get; private set; }
+    public float StartTime { get; set; }
+
+    void Awake()
+    {
+        StartTime = 4f;
+        CurrentTime = StartTime;
+        IsCounting = false;
+    }
+
+    void Update()
+    {
+        if (IsCounting)
+        {
+            CurrentTime -= Time.deltaTime;
+
+            if (CurrentTime <= 0.0000f)
+            {
+                ResetTimer();
+
+                IsCounting = false;
+                Messenger.Broadcast(GameEvents.TIMER_STOP);
+            }
+        }
+    }
+
+    public void PauseTimer() => IsCounting = false;
+    public void ResumeTimer() => IsCounting = true;
+    public void ResetTimer() => CurrentTime = 0.0000f;
+    public void StartTimer()
+    {
+        CurrentTime = StartTime;
+        IsCounting = true;
+    }
+}
