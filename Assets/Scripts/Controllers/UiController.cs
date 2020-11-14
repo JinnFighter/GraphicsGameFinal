ccdf1b66065d77069120f2b797fad1ca3773d5e7
Jinn <chernayamolnia@yandex.ru>
2020-11-14 22:01:41 +0400

    added new UiController class

---
+++ Assets/Scripts/Controllers/UiController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class UiController : Controller
+{
+    [SerializeField] private GameObject _settingsPopup;
+
+    public override void Notify(string eventType)
+    {
+        if(eventType == GameEvents.PAUSE_GAME)
+        {
+            _settingsPopup.SetActive(true);
+        }
+        else if(eventType == GameEvents.CONTINUE_GAME)
+        {
+            _settingsPopup.SetActive(false);
+        }
+    }
+}
