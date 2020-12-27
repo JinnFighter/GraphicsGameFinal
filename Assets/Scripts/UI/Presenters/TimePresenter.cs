8d01ff183e648091af5096fc99f7b1a057124898
Jinn <chernayamolnia@yandex.ru>
2020-12-27 11:04:25 +0300

    added TimePresenter class

---
+++ Assets/Scripts/UI/Presenters/TimePresenter.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TimePresenter : MonoBehaviour, ITimePresenter
+{
+    [SerializeField] private List<ITextView> _views;
+    private TimerComponent _timer;
+    private TimerFormat _timerFormat;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _timer = GetComponent<TimerComponent>();
+        _timerFormat = GetComponent<TimerFormat>();
+    }
+
+    public void OnTimeChange(float time) => _timerFormat.GetFormattedTime(time);
+}
