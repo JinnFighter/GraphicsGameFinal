c9db15f2db5a60711187e47043f33875d769dd0a
Jinn <chernayamolnia@yandex.ru>
2020-12-27 11:00:19 +0300

    added TextView class

---
+++ Assets/Scripts/UI/Views/TextView.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TextView : MonoBehaviour, ITextView
+{
+    private Text _text;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _text = GetComponent<Text>();
+    }
+
+    public void SetText(string text) => _text.text = text;
+}
