38d5fa91bc2bfdd7870259d02d2bdfff1eaa1e07
Jinn <chernayamolnia@yandex.ru>
2020-12-27 11:59:33 +0300

    added TextScorePresenter class

---
+++ Assets/Scripts/UI/Presenters/TextScorePresenter.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class TextScorePresenter : MonoBehaviour, IScorePresenter
+{
+    [SerializeField] private List<TextView> _views;
+    private ScoreKeeper _scoreKeeper;
+
+    public void OnScoreChanged(int currentScore)
+    {
+        foreach(var view in _views)
+            view.SetText(currentScore.ToString());
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _scoreKeeper = GetComponent<ScoreKeeper>();
+        _scoreKeeper.ScoreChangedEvent += OnScoreChanged;
+    }
+
+    void OnDestroy()
+    {
+        _scoreKeeper.ScoreChangedEvent -= OnScoreChanged;
+    }
+}
