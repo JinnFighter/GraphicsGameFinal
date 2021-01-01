aa30b5c35d6dc22ab3675cd8db46349d532634c7
a <b>
2021-01-01 17:51:47 +0300

    BrezenheimMode class now inherits GameMode class

--- Assets/Scripts/GameModes/Brezenheim/BrezenheimMode.cs
+++ Assets/Scripts/GameModes/Brezenheim/BrezenheimMode.cs
@@ -1,12 +1,12 @@
 using System.Collections.Generic;
 
-public class BrezenheimMode
+public class BrezenheimMode : GameMode
 {
     protected int curLine;
     protected List<LinesModeData> linesDatas;
     protected List<int>[] ds;
 
-    public BrezenheimMode()
+    public BrezenheimMode(int difficulty) : base(difficulty) 
     {
         curLine = 0;
         linesDatas = new List<LinesModeData>();
@@ -16,7 +16,7 @@ public class BrezenheimMode
     {
         var isGameOver = false;
         var isCorrect = false;
-        var d = 0;
+        var d = ds[curLine][linesDatas[curLine].GetCurrentIndex()];
         var points = new List<Position>();
         var canClearGrid = false;
 
@@ -49,4 +49,56 @@ public class BrezenheimMode
 
         return new BrezenheimModeData(isGameOver, isCorrect, d, points, canClearGrid);
     }
+
+    protected virtual void GenerateLines()
+    {
+        int minLength;
+        int maxLength;
+        int maxLengthSum;
+        int linesCount;
+        switch (difficulty)
+        {
+            case 1:
+                linesCount = 7;
+                minLength = 4;
+                maxLength = 8;
+                maxLengthSum = 48;
+                break;
+            case 2:
+                linesCount = 10;
+                minLength = 5;
+                maxLength = 10;
+                maxLengthSum = 90;
+                break;
+            default:
+                linesCount = 5;
+                minLength = 2;
+                maxLength = 5;
+                maxLengthSum = 20;
+                break;
+        }
+        var lineGenerator = new RandomLineGenerator(minLength, maxLength, maxLengthSum);
+        var lines = lineGenerator.Generate(linesCount);
+
+        ds = new List<int>[linesCount];
+        linesDatas = new List<LinesModeData>(linesCount);
+
+        for (var i = 0; i < lines.Count; i++)
+        {
+            var linePoints = Algorithms.GetBrezenheimLineData(lines[i], out var coeffs);
+            ds[i] = new List<int>();
+            linesDatas.Add(new LinesModeData());
+            ds[i].AddRange(coeffs);
+            linesDatas[i].AddRange(linePoints);
+        }
+    }
+
+    public int GetDIndex() => ds[curLine][linesDatas[curLine].GetCurrentIndex()];
+
+    public override void Check(Pixel invoker)
+    {
+        
+    }
+
+    public override void DoRestartAction() => GenerateLines();
 }
