c5c599d1b12b4ed329012d90e9ee7d9bbceafaaa
a <b>
2021-02-07 20:06:28 +0300

    added GameplayEventEvent

--- Assets/Scripts/GameModeController.cs
+++ Assets/Scripts/GameModeController.cs
@@ -5,4 +5,8 @@ public abstract class GameModeController : MonoBehaviour
     public NewGameMode GameMode { get; protected set; }
 
     public abstract void Check(Pixel invoker);
+
+    public delegate void GameEvent(string eventType);
+
+    public event GameEvent GameEventGenerated;
 }
