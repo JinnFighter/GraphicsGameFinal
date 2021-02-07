78256b9db0c4ab477471714f47821e08925d8b53
a <b>
2021-02-07 18:36:27 +0300

    added EndgameGameplayState class

---
+++ Assets/Scripts/GameplayStates/EndgameGameplayState.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class EndgameGameplayState : GameplayState
+{
+    [SerializeField] private GameOverPresenter _endgameScreen;
+    [SerializeField] private StatTrackerController _statTracker;
+
+    public override void Init()
+    {
+        _endgameScreen.gameObject.SetActive(true);
+        _endgameScreen.UpdateData(_statTracker.GetData());
+    }
+
+    public override void OnDelete()
+    {
+        _endgameScreen.gameObject.SetActive(false);
+        _statTracker.ResetData();
+    }
+}
