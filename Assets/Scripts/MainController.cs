75282353981f1f55d884734fd27615daaad50901
a <b>
2021-02-07 23:07:50 +0300

    added proper unpause logic and fixed incorrect unpause logic

--- Assets/Scripts/MainController.cs
+++ Assets/Scripts/MainController.cs
@@ -1,8 +1,10 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MainController : MonoBehaviour, IPausable
 {
     [SerializeField] private StatesContainer _statesContainer;
+    [SerializeField] List<Controller> _controllers;
 
     // Start is called before the first frame update
     void Start()
@@ -10,7 +12,21 @@ public class MainController : MonoBehaviour, IPausable
         _statesContainer.OnStartEvent();
     }
 
-    public void Pause() => _statesContainer.Pause();
+    public void Pause()
+    {
+        _statesContainer.Pause();
+        Notify(GameEvents.PAUSE_GAME);
+    }
 
-    public void Unpause() => _statesContainer.Unpause();
+    public void Unpause()
+    {
+        _statesContainer.Unpause();
+        Notify(GameEvents.CONTINUE_GAME);
+    }
+
+    private void Notify(string eventType)
+    {
+        foreach (var controller in _controllers)
+            controller.Notify(eventType);
+    }
 }
