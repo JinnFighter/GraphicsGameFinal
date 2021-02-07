05df6c573872e2fe081ba6c5a4c539b35f741f97
a <b>
2021-02-07 23:23:39 +0300

    added stopwatch to stat tracker

--- Assets/Scripts/StatTracker/StatTrackerController.cs
+++ Assets/Scripts/StatTracker/StatTrackerController.cs
@@ -1,9 +1,12 @@
-public class StatTrackerController : Controller
+public class StatTrackerController : Controller, IPausable
 {
+    private StopwatchComponent _stopwatch;
     private RoundStatsData _roundStatsData;
 
     void Start()
     {
+        _stopwatch = GetComponent<StopwatchComponent>();
+        _stopwatch.TimeChange += OnStopwatchTick;
         _roundStatsData = new RoundStatsData();
         actions.Add(GameEvents.ACTION_RIGHT_ANSWER, _roundStatsData.AddCorrentAnswer);
         actions.Add(GameEvents.ACTION_WRONG_ANSWER, _roundStatsData.AddWrongAnswer);
@@ -12,5 +15,21 @@ public class StatTrackerController : Controller
 
     public RoundStatsData GetData() => _roundStatsData;
 
-    public void ResetData() => _roundStatsData.Reset();
+    public void ResetData()
+    {
+        _stopwatch.Stop();
+        _stopwatch.ResetStopwatch();
+        _roundStatsData.Reset();
+    }
+
+    public void Pause() => _stopwatch.Stop();
+
+    public void Unpause() => _stopwatch.Resume();
+
+    private void OnStopwatchTick(float time) => _roundStatsData.SetTimeSpent(time);
+
+    void OnDestroy()
+    {
+        _stopwatch.TimeChange -= OnStopwatchTick;
+    }
 }
