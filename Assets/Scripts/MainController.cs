cd9919c78481f3f25df54f27c91d9f2fee7ec880
a <b>
2021-02-23 22:09:49 +0300

    fixed incorrect logic of tutorial image when person enters some mode first time

--- Assets/Scripts/MainController.cs
+++ Assets/Scripts/MainController.cs
@@ -1,16 +1,24 @@
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainController : MonoBehaviour, IPausable
 {
+    [SerializeField] private GameObject _tutorialScreen;
     [SerializeField] private UiScreenContainer _uiScreenContainer;
     [SerializeField] private StatesContainer _statesContainer;
     [SerializeField] List<Controller> _controllers;
+    [SerializeField] private ProfilesManager _profilesManager;
 
     // Start is called before the first frame update
     void Start()
     {
         _statesContainer.OnStartEvent();
+        if(IsFirstTimePlaying())
+        {
+            _statesContainer.Pause();
+            _uiScreenContainer.Push(_tutorialScreen);
+        }
     }
 
 
@@ -45,5 +53,19 @@ public class MainController : MonoBehaviour, IPausable
     {
         foreach (var controller in _controllers)
             controller.Notify(eventType);
+    }
+
+    private bool IsFirstTimePlaying()
+    {
+        var prefId = _profilesManager.ActiveProfile.name + "_" + SceneManager.GetActiveScene().name + "_first_visit";
+        var visitState = PlayerPrefs.HasKey(prefId) ? PlayerPrefs.GetInt(prefId) : 1;
+        if (visitState == 1)
+        {
+            PlayerPrefs.SetInt(prefId, 0);
+            PlayerPrefs.Save();
+            return true;
+        }
+
+        return false;
     }
 }
