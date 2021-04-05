1d6651382a70f5266edc3a5d649e71a1ac1b248a
a <b>
2021-04-05 20:18:31 +0300

    add LaunchGameplayTimerSystem

---
+++ Assets/Scripts/Systems/LaunchGameplayTimerSystem.cs
@@ -0,0 +1,22 @@
+using Leopotam.Ecs;
+
+namespace Pixelgrid 
+{
+    public sealed class LaunchGameplayTimerSystem : IEcsRunSystem 
+    {
+        private EcsFilter<Timer, GameplayTimerComponent>.Exclude<Counting> _timersFilter;
+        private EcsFilter<StartGameEvent> _startGameFilter;
+        
+        void IEcsRunSystem.Run() 
+        {
+            if(!_startGameFilter.IsEmpty())
+            {
+                foreach(var index in _timersFilter)
+                {
+                    var entity = _timersFilter.GetEntity(index);
+                    entity.Get<Counting>();
+                }
+            }
+        }
+    }
+}
