0c78704a243241faba10c187e86f07e39649f8f6
a <b>
2021-04-06 19:34:54 +0300

    add CreateStatDataTrackerSystem to Brezenheim's systems

--- Assets/Scripts/Startups/BrezenheimStartup.cs
+++ Assets/Scripts/Startups/BrezenheimStartup.cs
@@ -29,6 +29,7 @@ namespace Pixelgrid {
                  .Add(new SetDifficultySystem())
                  .Add(new GenerateGameFieldSystem())
                  .Add(new GenerateTimersSystem())
+                 .Add(new CreateStatDataTrackerSystem())
                  .Add(new SetGameplayTimerStartTimeSystem())
                  .Add(new StartGameSystem())
                  .Add(new LaunchGameplayTimerSystem())
@@ -40,7 +41,6 @@ namespace Pixelgrid {
                  .Add(new CheckClickSystem())
                  .Add(new UpdatePixelSystem())
                  .InjectUi(_ecsUiEmitter)
-                // .Add (new TestSystem2 ())
 
                 // register one-frame components (order is important), for example:
                  .OneFrame<PixelClickedEvent>()
@@ -49,7 +49,6 @@ namespace Pixelgrid {
                  .OneFrame<CorrectAnswerEvent>()
                  .OneFrame<WrongAnswerEvent>()
                  .OneFrame<GameOverEvent>()
-                // .OneFrame<TestComponent2> ()
 
                 // inject service instances here (order doesn't important), for example:
                 .Inject(difficultyConfiguration)
