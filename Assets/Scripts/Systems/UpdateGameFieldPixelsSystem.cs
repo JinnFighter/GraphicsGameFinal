30798c21fcfaac60f0f1ec07931e94fd991ec0ad
a <b>
2021-04-07 17:36:55 +0300

    add UpdateGameFieldPixelsSystem

---
+++ Assets/Scripts/Systems/UpdateGameFieldPixelsSystem.cs
@@ -0,0 +1,31 @@
+using Leopotam.Ecs;
+using UnityEngine.UI;
+
+namespace Pixelgrid 
+{
+    public sealed class UpdateGameFieldPixelsSystem : IEcsRunSystem 
+    {
+        private EcsFilter<PixelComponent, PixelPosition, PixelRef> _pixelsFilter;
+        private EcsFilter<LineDrawData> _drawDataFilter;
+
+        void IEcsRunSystem.Run() 
+        {
+            foreach(var index in _drawDataFilter)
+            {
+                var drawData = _drawDataFilter.Get1(index);
+                var data = drawData.drawData;
+                foreach(var pixelIndex in _pixelsFilter)
+                {
+                    foreach(var item in data)
+                    {
+                        if(item.Item1 == _pixelsFilter.Get2(index).position)
+                        {
+                            ref var pixelRef = ref _pixelsFilter.Get3(index);
+                            pixelRef.pixel.GetComponent<Image>().sprite = item.Item2;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
