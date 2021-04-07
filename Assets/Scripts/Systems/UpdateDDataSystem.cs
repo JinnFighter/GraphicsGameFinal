2ce16dbf67624e3838fa2fdc63d4fb111cfd5096
a <b>
2021-04-07 20:43:17 +0300

    add UpdateDDataSystem

---
+++ Assets/Scripts/Systems/UpdateDDataSystem.cs
@@ -0,0 +1,24 @@
+using Leopotam.Ecs;
+using UnityEngine.UI;
+
+namespace Pixelgrid 
+{
+    public sealed class UpdateDDataSystem : IEcsRunSystem 
+    {
+        private EcsFilter<GameModeData, TextRef> _filter;
+        private EcsFilter<GameplayEventReceiver, UpdateDIndexEvent> _eventFilter;
+
+        void IEcsRunSystem.Run() 
+        {
+            if(!_eventFilter.IsEmpty())
+            {
+                var indexEvent = _eventFilter.Get2(0);
+                foreach(var index in _filter)
+                {
+                    ref var textRef = ref _filter.Get2(index);
+                    textRef.Text.GetComponent<Text>().text = indexEvent.index >= 0 ? "-" : "+";
+                }
+            }
+        }
+    }
+}
