2010d2b206e5268e029a0e73a6aae5115af05b72
a <b>
2021-04-07 17:07:57 +0300

    add proper RandomLinesGenerator generation logic

--- Assets/Scripts/Brezenheim/RandomLinesGenerator.cs
+++ Assets/Scripts/Brezenheim/RandomLinesGenerator.cs
@@ -14,7 +14,34 @@ namespace Pixelgrid
 
         public override IEnumerable<(Vector2Int, Vector2Int)> GenerateData(int minLength, int maxLength, int count)
         {
-            return new List<(Vector2Int, Vector2Int)>();
+            var lines = new List<(Vector2Int, Vector2Int)>();
+            var maxLengthSum = MaxLengthSum;
+
+            for(var i = 0; i < count; i++)
+            {
+                if(maxLengthSum > 0)
+                {
+                    var length = 0;
+                    int x0 = Random.Range(0, 9);
+                    int y0 = Random.Range(0, 9);
+                    int x1 = Random.Range(0, 9);
+                    int y1 = Random.Range(0, 9);
+                    while(length < minLength || length > maxLength)
+                    {
+                        x0 = Random.Range(0, 9);
+                        y0 = Random.Range(0, 9);
+                        x1 = Random.Range(0, 9);
+                        y1 = Random.Range(0, 9);
+                        var line = new NewLine(new Vector2Int(x0, y0), new Vector2Int(x1, y1));
+                        length = (int)line.GetLength();
+                    }
+
+                    lines.Add((new Vector2Int(x0, y0), new Vector2Int(x1, y1)));
+                    maxLengthSum -= length;
+                }
+            }
+
+            return lines;
         }
     }
 }
