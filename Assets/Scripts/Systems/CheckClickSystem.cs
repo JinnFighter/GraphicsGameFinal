1c170c9e750f36f35af6da0de8212cffe92b887e
a <b>
2021-04-14 17:12:15 +0300

    CheckClickSystem now works only if there is no pause

--- Assets/Scripts/Systems/CheckClickSystem.cs
+++ Assets/Scripts/Systems/CheckClickSystem.cs
@@ -6,16 +6,20 @@ namespace Pixelgrid
     public sealed class CheckClickSystem : IEcsRunSystem 
     {
         private EcsFilter<EcsUiClickEvent> _filter;
+        private EcsFilter<GameplayEventReceiver, PauseEvent> _pauseFilter;
 
         void IEcsRunSystem.Run() 
         {
-            foreach (var index in _filter)
+            if(_pauseFilter.IsEmpty())
             {
-                ref EcsUiClickEvent data = ref _filter.Get1(index);
-                var sender = data.Sender;
-                var pixel = sender.GetComponent<GridPixel>();
-                if(pixel)
-                    pixel.entity.Get<PixelClickedEvent>();
+                foreach (var index in _filter)
+                {
+                    ref EcsUiClickEvent data = ref _filter.Get1(index);
+                    var sender = data.Sender;
+                    var pixel = sender.GetComponent<GridPixel>();
+                    if (pixel)
+                        pixel.entity.Get<PixelClickedEvent>();
+                }
             }
         }
     }
