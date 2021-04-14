37e3f530a944b345ce69b91a4b2b35821d2aaff9
a <b>
2021-04-14 16:38:07 +0300

    UpdateTimersSystem now checks for pause events before updating

--- Assets/Scripts/Systems/UpdateTimersSystem.cs
+++ Assets/Scripts/Systems/UpdateTimersSystem.cs
@@ -7,29 +7,33 @@ namespace Pixelgrid
     public sealed class UpdateTimersSystem : IEcsRunSystem 
     {
         private EcsFilter<Timer, Counting, TimerRef> _filter;
+        private EcsFilter<GameplayEventReceiver, PauseEvent> _pauseFilter;
         
         void IEcsRunSystem.Run () 
         {
-            foreach(var index in _filter)
+            if(_pauseFilter.IsEmpty())
             {
-                ref var timerData = ref _filter.Get1(index);
-                var currentTime = timerData.currentTime -= Time.deltaTime;
-
-                if (currentTime <= 0.000f)
-                    currentTime = 0.000f;
-
-                timerData.currentTime = currentTime;
-
-                var timerRef = _filter.Get3(index);
-                var timerGameObject = timerRef.timer;
-                var timerText = timerGameObject.GetComponent<Text>();
-                var timerFormat = timerGameObject.GetComponent<TimerFormat>();
-                timerText.text = timerFormat.GetFormattedTime(currentTime);
-                if (timerData.currentTime <= 0.0000f)
+                foreach (var index in _filter)
                 {
-                    var entity = _filter.GetEntity(index);
-                    entity.Del<Counting>();
-                    entity.Get<TimerEndEvent>();
+                    ref var timerData = ref _filter.Get1(index);
+                    var currentTime = timerData.currentTime -= Time.deltaTime;
+
+                    if (currentTime <= 0.000f)
+                        currentTime = 0.000f;
+
+                    timerData.currentTime = currentTime;
+
+                    var timerRef = _filter.Get3(index);
+                    var timerGameObject = timerRef.timer;
+                    var timerText = timerGameObject.GetComponent<Text>();
+                    var timerFormat = timerGameObject.GetComponent<TimerFormat>();
+                    timerText.text = timerFormat.GetFormattedTime(currentTime);
+                    if (timerData.currentTime <= 0.0000f)
+                    {
+                        var entity = _filter.GetEntity(index);
+                        entity.Del<Counting>();
+                        entity.Get<TimerEndEvent>();
+                    }
                 }
             }
         }
