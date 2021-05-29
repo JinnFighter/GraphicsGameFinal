9276db54e6198242e41ed6b2da0ae819009256a4
a <b>
2021-05-29 17:08:53 +0300

    add AudioPlayer

---
+++ Assets/Scripts/Controllers/AudioPlayer.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+namespace Pixelgrid
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class AudioPlayer : MonoBehaviour
+    {
+        private AudioSource _source;
+
+        void Awake()
+        {
+            _source = GetComponent<AudioSource>();
+
+        }
+
+        public void PlayClip(AudioClip audioClip) => _source.PlayOneShot(audioClip);
+    }
+}
