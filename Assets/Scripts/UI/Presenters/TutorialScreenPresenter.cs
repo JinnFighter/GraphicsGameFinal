b09f27569a9c0a32348acc4b2009a2488ba49aca
a <b>
2021-05-29 20:47:01 +0300

    add TutorialScreenPresenter.SetText method

--- Assets/Scripts/UI/Presenters/TutorialScreenPresenter.cs
+++ Assets/Scripts/UI/Presenters/TutorialScreenPresenter.cs
@@ -6,5 +6,7 @@ namespace Pixelgrid
     public class TutorialScreenPresenter : MonoBehaviour
     {
         [SerializeField] private Text _text;
+
+        public void SetText(string text) => _text.text = text;
     }
 }
