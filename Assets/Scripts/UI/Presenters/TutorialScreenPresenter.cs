1279cb92b06496a4fcc8b4c6c0ef0424578f38dc
a <b>
2021-05-29 19:18:55 +0300

    add TutorialScreenPresenter

---
+++ Assets/Scripts/UI/Presenters/TutorialScreenPresenter.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Pixelgrid
+{
+    public class TutorialScreenPresenter : MonoBehaviour
+    {
+        [SerializeField] private Text _text;
+    }
+}
