5a589b66d4184b30ae39971618ef251e1588988c
a <b>
2021-06-14 19:05:32 +0300

    add GenerateBezierDataSystem

---
+++ Assets/Scripts/Systems/GameModes/Bezier/GenerateBezierDataSystem.cs
@@ -0,0 +1,28 @@
+using Leopotam.Ecs;
+using System.Linq;
+
+namespace Pixelgrid
+{
+    public sealed class GenerateBezierDataSystem : IEcsRunSystem
+    {
+        private EcsFilter<GameModeData> _gameModeDataFilter;
+        private BezierLinesGenerator _lineDataGenerator;
+        private EcsFilter<RestartGameEvent> _restartEventFilter;
+
+        public void Run()
+        {
+            if (!_restartEventFilter.IsEmpty())
+            {
+                foreach (var index in _gameModeDataFilter)
+                {
+                    var entity = _gameModeDataFilter.GetEntity(index);
+                    ref var lineData = ref entity.Get<BezierLineData>();
+                    var lines = _lineDataGenerator.GenerateData(3, 5, 3).ToList();
+
+                    lineData.Points = lines;
+                    lineData.CurrentPoint = 0;
+                }
+            }
+        }
+    }
+}
