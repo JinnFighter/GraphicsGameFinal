3a23f0ac0538aee9e6f14b8e793e3c37fce7b0e5
Jinn <chernayamolnia@yandex.ru>
2021-06-15 22:37:36 +0300

    add OnSliderValueChangeSystem

---
+++ Assets/Scripts/Systems/GameModes/ColorPicker/OnSliderValueChangeSystem.cs
@@ -0,0 +1,29 @@
+using Leopotam.Ecs;
+using Leopotam.Ecs.Ui.Components;
+
+namespace Pixelgrid 
+{
+    public sealed class OnSliderValueChangeSystem : IEcsRunSystem 
+    {
+        private EcsFilter<EcsUiSliderChangeEvent> _filter;
+        private GameState _gameState;
+        private ImageHolderContainer _imageHolderContainer;
+
+        void IEcsRunSystem.Run() 
+        {
+            if(!_gameState.IsPaused)
+            {
+                foreach (var index in _filter)
+                {
+                    ref EcsUiSliderChangeEvent data = ref _filter.Get1(index);
+                    var sender = data.Sender;
+                    if(sender.CompareTag("ColorCheckButton"))
+                    {
+                        var color = _imageHolderContainer.AnswerHolder.color;
+                        color.b = data.Value;
+                    }
+                }
+            }
+        }
+    }
+}
