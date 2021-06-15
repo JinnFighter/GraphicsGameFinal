86cf055661beee4149b6ec99ac82c4306b46e7da
Jinn <chernayamolnia@yandex.ru>
2021-06-15 18:27:18 +0300

    add ColorPickerData component

---
+++ Assets/Scripts/Components/GameModes/ColorPicker/ColorPickerData.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Pixelgrid 
+{
+    public struct ColorPickerData 
+    {
+        public List<Color32> Colours;
+        public int CurrentColour;
+    }
+}
