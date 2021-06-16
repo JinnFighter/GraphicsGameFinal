d998d2f560f5ed49b9510bffae87c1aa309fa876
a <b>
2021-06-16 18:29:44 +0300

    reduced usages of ICommand interface

--- Assets/Scripts/Systems/GameModes/Brezenheim/GenerateDDataSystem.cs
+++ Assets/Scripts/Systems/GameModes/Brezenheim/GenerateDDataSystem.cs
@@ -2,26 +2,23 @@ using Leopotam.Ecs;
 
 namespace Pixelgrid 
 {
-    public sealed class GenerateDDataSystem : IEcsRunSystem, ICommand 
+    public sealed class GenerateDDataSystem : IEcsRunSystem
     {
         private EcsFilter<GameModeData> _filter;
         private BrezenheimDataContainer _brezenheimDataContainer;
         private EcsFilter<RestartGameEvent> _restartEventFilter;
 
-        public void Execute()
-        {
-            foreach (var index in _filter)
-            {
-                var entity = _filter.GetEntity(index);
-                ref var dData = ref entity.Get<TextRef>();
-                dData.Text = _brezenheimDataContainer.DText.gameObject;
-            }
-        }
-
         public void Run()
         {
             if (!_restartEventFilter.IsEmpty())
-                Execute();
+            {
+                foreach (var index in _filter)
+                {
+                    var entity = _filter.GetEntity(index);
+                    ref var dData = ref entity.Get<TextRef>();
+                    dData.Text = _brezenheimDataContainer.DText.gameObject;
+                }
+            }
         }
     }
 }
