6de71d3304747d0874ad666214a69c88d2433387
a <b>
2021-06-16 10:20:17 +0300

    add BorderComponent

---
+++ Assets/Scripts/Components/GameModes/SouthCohen/BorderComponent.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+namespace Pixelgrid 
+{
+    public struct BorderComponent 
+    {
+        public Vector2Int TopLeftCorner;
+        public Vector2Int DownRightCorner;
+    }
+}
