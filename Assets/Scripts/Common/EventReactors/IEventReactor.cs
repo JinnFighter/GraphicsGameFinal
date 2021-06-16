6cfa51a6ca1ebb6f3871afad4bdd7b6f379d04b9
a <b>
2021-06-16 17:40:18 +0300

    rem EventReactors

--- Assets/Scripts/Common/EventReactors/IEventReactor.cs
+++
@@ -1,9 +0,0 @@
-﻿public interface IEventReactor
-{
-    void OnGameOver();
-    void OnChangeState(int difficulty);
-    void OnRestart();
-    void OnPause();
-    void OnContinue();
-    bool CanCheckAction();
-}
