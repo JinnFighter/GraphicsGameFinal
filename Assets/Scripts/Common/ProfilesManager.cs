abf46236d993833819a8dbe97fcda2943497be5b
a <b>
2021-06-20 18:14:56 +0300

    fix creation of players_base file

--- Assets/Scripts/Common/ProfilesManager.cs
+++ Assets/Scripts/Common/ProfilesManager.cs
@@ -1,15 +1,32 @@
-﻿using System.Linq;
+﻿using System.IO;
+using System.Linq;
+using System.Xml;
 using UnityEngine;
 
 public class ProfilesManager : MonoBehaviour
 {
-    public static string path = "Data/players_base.xml";
+    public static string path = Path.Combine("Data", "players_base.xml");
 
     public PlayerProfile ActiveProfile { get; set; }
     public PlayerProfilesContainer Container { get; set; }
 
     public void Load()
     {
+        if (!Directory.Exists("Data"))
+            Directory.CreateDirectory("Data");
+        if (!File.Exists(path))
+        {
+            using (XmlWriter writer = XmlWriter.Create(path))
+            {
+                var text = "<?xml version=\"1.0\"?><PlayersCollection xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\"><Players></Players></PlayersCollection>".ToArray();
+
+                writer.WriteStartElement("PlayersCollection");
+                writer.WriteStartElement("Players");
+                writer.WriteEndElement();
+                writer.WriteEndElement();
+                writer.Close();
+            }; 
+        }
         Container = PlayerProfilesContainer.Load(path);
 
         if(Container.profiles.Any(profile => profile.active))
