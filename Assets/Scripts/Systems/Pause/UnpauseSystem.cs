ea7a76dc3927cb0573374d48eb5ef96f701475ee
JinnFighter <chernayamolnia@yandex.ru>
2021-08-07 18:27:51 +0300

    add UnpauseSystem

---
+++ Assets/Scripts/Systems/Pause/UnpauseSystem.cs
@@ -0,0 +1,28 @@
+using Leopotam.Ecs;
+using System.Collections.Generic;
+
+namespace Pixelgrid
+{
+    public sealed class UnpauseSystem : IEcsRunSystem
+    {
+        private readonly EcsFilter<PauseEvent> _filter = null;
+
+        private readonly EcsSystems _systems;
+        private readonly IEnumerable<string> _pausableSystemsNames;
+
+        public UnpauseSystem(EcsSystems systems, IEnumerable<string> pausableSystemsNames)
+        {
+            _systems = systems;
+            _pausableSystemsNames = pausableSystemsNames;
+        }
+
+        void IEcsRunSystem.Run()
+        {
+            if (!_filter.IsEmpty())
+            {
+                foreach (var systemName in _pausableSystemsNames)
+                    _systems.SetRunSystemState(_systems.GetNamedRunSystem(systemName), true);
+            }
+        }
+    }
+}
