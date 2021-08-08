0bdc113107c0af0a4825b131a70f24c21f5af797
JinnFighter <chernayamolnia@yandex.ru>
2021-08-08 08:38:04 +0300

    add TextRef component to GenerateCountdownTimersSystem

--- Assets/Scripts/Systems/Timers/GenerateCountdownTimersSystem.cs
+++ Assets/Scripts/Systems/Timers/GenerateCountdownTimersSystem.cs
@@ -1,4 +1,5 @@
 using Leopotam.Ecs;
+using UnityEngine.UI;
 
 namespace Pixelgrid {
     public sealed class GenerateCountdownTimersSystem : IEcsInitSystem 
@@ -13,6 +14,8 @@ namespace Pixelgrid {
             timerEntity.Get<CountdownTimer>();
             ref var timerRef = ref timerEntity.Get<TimerRef>();
             timerRef.timer = _timersContainer.CountdownTimer;
+            ref var textRef = ref timerEntity.Get<TextRef>();
+            textRef.Text = _timersContainer.CountdownTimer.GetComponent<Text>();
         }
     }
 }
