292c8fd6f54260aa2c28a9dcc8281425b70e2f01
JinnFighter <chernayamolnia@yandex.ru>
2021-08-29 21:00:23 +0300

    rem GameModeData component usage from Bezier-related systems

--- Assets/Scripts/Systems/GameModes/Bezier/GenerateBezierDataSystem.cs
+++ Assets/Scripts/Systems/GameModes/Bezier/GenerateBezierDataSystem.cs
@@ -5,7 +5,7 @@ namespace Pixelgrid
 {
     public sealed class GenerateBezierDataSystem : IEcsRunSystem
     {
-        private EcsFilter<GameModeData> _gameModeDataFilter;
+        private EcsFilter<BezierLineData> _gameModeDataFilter;
         private BezierLinesGenerator _lineDataGenerator;
         private EcsFilter<RestartGameEvent> _restartEventFilter;
 
@@ -16,7 +16,7 @@ namespace Pixelgrid
                 foreach (var index in _gameModeDataFilter)
                 {
                     var entity = _gameModeDataFilter.GetEntity(index);
-                    ref var lineData = ref entity.Get<BezierLineData>();
+                    ref var lineData = ref _gameModeDataFilter.Get1(index);
                     var lines = _lineDataGenerator.GenerateData(3, 5, 3).ToList();
 
                     lineData.Points = lines;
