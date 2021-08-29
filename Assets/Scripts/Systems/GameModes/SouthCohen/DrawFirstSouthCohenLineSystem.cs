f2d4259eb787cf41c1bc9defd928103fd11fff07
JinnFighter <chernayamolnia@yandex.ru>
2021-08-29 21:10:19 +0300

    fix filters having same components in different order

--- Assets/Scripts/Systems/GameModes/SouthCohen/DrawFirstSouthCohenLineSystem.cs
+++ Assets/Scripts/Systems/GameModes/SouthCohen/DrawFirstSouthCohenLineSystem.cs
@@ -6,7 +6,7 @@ namespace Pixelgrid
     public sealed class DrawFirstSouthCohenLineSystem : IEcsRunSystem 
     {
         private readonly EcsFilter<RestartGameEvent> _filter = null;
-        private readonly EcsFilter<SouthCohenData, LineData> _dataFilter = null;
+        private readonly EcsFilter<LineData, SouthCohenData> _dataFilter = null;
         
         private readonly SpritesContainer _spritesContainer = null;
 
@@ -17,7 +17,7 @@ namespace Pixelgrid
                 foreach (var index in _dataFilter)
                 {
                     var entity = _dataFilter.GetEntity(index);
-                    var data = _dataFilter.Get2(index);
+                    var data = _dataFilter.Get1(index);
                     entity.Get<ClearGridEvent>();
                     ref var drawDataComponent = ref entity.Get<LineDrawData>();
                     var drawingData = 
