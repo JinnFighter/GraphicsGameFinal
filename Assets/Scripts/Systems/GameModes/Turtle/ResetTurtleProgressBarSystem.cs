51318c543515ce90b2adef3e3b4d64c0b63ad687
JinnFighter <chernayamolnia@yandex.ru>
2021-08-29 16:09:11 +0300

    ResetTurtleProgressBarSystem now uses ProgressBar as service

--- Assets/Scripts/Systems/GameModes/Turtle/ResetTurtleProgressBarSystem.cs
+++ Assets/Scripts/Systems/GameModes/Turtle/ResetTurtleProgressBarSystem.cs
@@ -6,20 +6,19 @@ namespace Pixelgrid
     public sealed class ResetTurtleProgressBarSystem : IEcsRunSystem 
     {
         private readonly EcsFilter<RestartGameEvent> _restartEventFilter = null;
-        private readonly EcsFilter<ProgressBarComponent> _progressBarFilter = null;
         private readonly EcsFilter<TurtlePath> _turtlePathFilter = null;
+
+        private readonly ProgressBar _progressBar;
 
         void IEcsRunSystem.Run()
         {
             if (!_restartEventFilter.IsEmpty())
             {
-                ref var turtlePath = ref _turtlePathFilter.Get1(0);
-                foreach (var index in _progressBarFilter)
+                foreach (var index in _turtlePathFilter)
                 {
-                    ref var progressBarComponent = ref _progressBarFilter.Get1(index);
-                    var progressBar = progressBarComponent.ProgressBar;
-                    progressBar.MaxValue = turtlePath.Path.Sum(turtlePath => turtlePath.Count);
-                    progressBar.CurrentValue = 0;
+                    var turtlePath = _turtlePathFilter.Get1(index);
+                    _progressBar.MaxValue = turtlePath.Path.Sum(path => path.Count);
+                    _progressBar.CurrentValue = 0;
                 }
             }
         }
