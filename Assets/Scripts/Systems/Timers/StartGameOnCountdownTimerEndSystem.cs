6362d934912ee20ecf8c0fb4bf27e095a6a515d7
JinnFighter <chernayamolnia@yandex.ru>
2021-08-29 14:10:42 +0300

    rem GameplayEventReceiver component from filter in StartGameOnCountdownTimerEndSystem

--- Assets/Scripts/Systems/Timers/StartGameOnCountdownTimerEndSystem.cs
+++ Assets/Scripts/Systems/Timers/StartGameOnCountdownTimerEndSystem.cs
@@ -4,18 +4,18 @@ namespace Pixelgrid
 {
     public sealed class StartGameOnCountdownTimerEndSystem : IEcsRunSystem 
     {
-        private CountdownScreenPresenter _countdownPresenter;
+        private readonly EcsFilter<Timer, CountdownTimer, TimerEndEvent> _timerFilter = null;
 
-        private EcsFilter<Timer, CountdownTimer, TimerEndEvent> _timerFilter;
-        private EcsFilter<GameplayEventReceiver> _eventReceiverFilter;
-
+        private readonly EcsWorld _world = null;
+        private readonly CountdownScreenPresenter _countdownPresenter = null;
+        
         void IEcsRunSystem.Run() 
         {
             if (!_timerFilter.IsEmpty())
             {
                 _countdownPresenter.gameObject.SetActive(false);
-                foreach (var index in _eventReceiverFilter)
-                    _eventReceiverFilter.GetEntity(index).Get<StartGameEvent>();
+                var entity = _world.NewEntity();
+                entity.Get<StartGameEvent>();
             }    
         }
     }
