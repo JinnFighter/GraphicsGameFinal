20785f6856a7bbd7c96f270b3276723f5cc2c104
JinnFighter <chernayamolnia@yandex.ru>
2021-08-29 17:58:10 +0300

    fix SouthCohen's LineZones generation

--- Assets/Scripts/Systems/GameModes/SouthCohen/GenerateLineZonesSystem.cs
+++ Assets/Scripts/Systems/GameModes/SouthCohen/GenerateLineZonesSystem.cs
@@ -46,47 +46,39 @@ namespace Pixelgrid
                             {
                                 Swap(ref ax, ref bx);
                                 Swap(ref ay, ref by);
-                                int c = code1;
-                                code1 = code2;
-                                code2 = c;
+                                Swap(ref code1,  ref code2);
                             }
 
                             if (Convert.ToBoolean(code1 & 0x01))
                             {
                                 ay += (left.x - ax) * (by - ay) / (bx - ax);
                                 ax = left.x;
-                                if (!zones[i].Contains(code1))
-                                    zones[i].Add(code1);
                             }
 
                             if (Convert.ToBoolean(code1 & 0x02))
                             {
                                 ax += (left.y - ay) * (bx - ax) / (by - ay);
                                 ay = left.y;
-                                if (!zones[i].Contains(code1))
-                                    zones[i].Add(code1);
                             }
 
                             if (Convert.ToBoolean(code1 & 0x04))
                             {
                                 ay += (right.x - ax) * (by - ay) / (bx - ax);
                                 ax = right.x;
-                                if (!zones[i].Contains(code1))
-                                    zones[i].Add(code1);
                             }
 
                             if (Convert.ToBoolean(code1 & 0x08))
                             {
                                 ax += (right.y - ay) * (bx - ax) / (by - ay);
                                 ay = right.y;
-                                if (!zones[i].Contains(code1))
-                                    zones[i].Add(code1);
                             }
 
                             code1 = _codeReceiver.GetCode(new UnityEngine.Vector2Int(ax, ay), left, right);
                             inside = (code1 | code2) == 0;
                             outside = (code1 & code2) != 0;
                         }
+                        if(!outside && ! zones[i].Contains(code1))
+                            zones[i].Add(code1);
                     }
 
                     zonesData.Zones = zones;
