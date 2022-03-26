d4ae6acfa32432b15a9935806d4c116073898573
JinnFighter <chernayamolnia@yandex.ru>
2022-03-26 15:10:37 +0300

    add: BrezenheimDataModel

---
+++ Assets/Scripts/DataModels/BrezenheimDataModel.cs
@@ -0,0 +1,9 @@
+using System.Collections.Generic;
+
+namespace Pixelgrid.DataModels
+{
+    public class BrezenheimDataModel
+    {
+        public List<List<int>> Indexes = new List<List<int>>();
+    }
+}
