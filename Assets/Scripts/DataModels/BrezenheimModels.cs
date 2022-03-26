cae83f574e49b1231101b4ade10fde0c6658f576
JinnFighter <chernayamolnia@yandex.ru>
2022-03-26 15:41:47 +0300

    add: BrezenheimModels

---
+++ Assets/Scripts/DataModels/BrezenheimModels.cs
@@ -0,0 +1,8 @@
+namespace Pixelgrid.DataModels
+{
+    public class BrezenheimModels
+    {
+        public BrezenheimIndexModel BrezenheimIndexModel { get; } = new BrezenheimIndexModel();
+        public LineDataModel LineDataModel { get; } = new LineDataModel();
+    }
+}
