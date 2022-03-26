a298ca9fbcce6f1700f8c59d5ce5ed162bfc05bb
JinnFighter <chernayamolnia@yandex.ru>
2022-03-26 18:09:36 +0300

    add: TurtleModels

---
+++ Assets/Scripts/DataModels/TurtlePathModel.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace Pixelgrid.DataModels
+{
+    public class TurtlePathModel
+    {
+        public List<List<char>> Path = new List<List<char>>();
+        public int CurrentPath;
+        public int CurrentSymbol;
+    }
+}
