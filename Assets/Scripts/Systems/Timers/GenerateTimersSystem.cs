f998dcb6897041f711b1d19c9e1f5f24213cbce9
JinnFighter <chernayamolnia@yandex.ru>
2022-04-05 20:48:26 +0300

    rem: ui code from GenerateTimersSystem

--- Assets/Scripts/Systems/Timers/GenerateTimersSystem.cs
+++ Assets/Scripts/Systems/Timers/GenerateTimersSystem.cs
@@ -1,25 +1,17 @@
 using Leopotam.Ecs;
 using Pixelgrid.Components.Time.Timer;
-using Pixelgrid.UI.Models;
-using Pixelgrid.UI.Presenters;
-using Pixelgrid.UI.Views;
 
 namespace Pixelgrid.Systems.Timers 
 {
     public sealed class GenerateTimersSystem : IEcsInitSystem 
     {
         private readonly EcsWorld _world = null;
-        private readonly TimersContainer _timersContainer = null;
-        
+
         public void Init() 
         {
             var timerEntity = _world.NewEntity();
-            ref var timer = ref timerEntity.Get<Timer>();
+            timerEntity.Get<Timer>();
             timerEntity.Get<GameplayTimerComponent>();
-            var timerModel = new TimerModel();
-            timer.TimeChangeEvent += timerModel.UpdateTime;
-
-            var presenter = new TimerPresenter(timerModel, _timersContainer.gameplayTimer.GetComponent<TimerView>());
         }
     }
 }
