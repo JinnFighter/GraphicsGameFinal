b5a53b1922d03d78b018a518de300a0eeb34c012
JinnFighter <chernayamolnia@yandex.ru>
2022-04-05 20:46:09 +0300

    rem: ui code from GenerateCountdownTimersSystem

--- Assets/Scripts/Systems/Timers/GenerateCountdownTimersSystem.cs
+++ Assets/Scripts/Systems/Timers/GenerateCountdownTimersSystem.cs
@@ -1,25 +1,17 @@
 using Leopotam.Ecs;
 using Pixelgrid.Components.Time.Timer;
-using Pixelgrid.UI.Models;
-using Pixelgrid.UI.Presenters;
-using Pixelgrid.UI.Views;
 
-namespace Pixelgrid.Systems.Timers {
+namespace Pixelgrid.Systems.Timers
+{
     public sealed class GenerateCountdownTimersSystem : IEcsInitSystem 
     {
         private readonly EcsWorld _world = null;
-        private readonly TimersContainer _timersContainer = null;
 
         public void Init()
         {
             var timerEntity = _world.NewEntity();
-            ref var timer = ref timerEntity.Get<Timer>();
+            timerEntity.Get<Timer>();
             timerEntity.Get<CountdownTimer>();
-            
-            var timerModel = new TimerModel();
-            timer.TimeChangeEvent += timerModel.UpdateTime;
-
-            var presenter = new TimerPresenter(timerModel, _timersContainer.CountdownTimer.GetComponent<TimerView>());
         }
     }
 }
