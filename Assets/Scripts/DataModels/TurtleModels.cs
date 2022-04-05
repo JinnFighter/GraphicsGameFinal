5e15d66b10aa69b155a77f47e43632684a208525
JinnFighter <chernayamolnia@yandex.ru>
2022-04-06 00:41:31 +0300

    add: AnswersModel to TurtleModels

--- Assets/Scripts/DataModels/TurtleModels.cs
+++ Assets/Scripts/DataModels/TurtleModels.cs
@@ -3,5 +3,6 @@ namespace Pixelgrid.DataModels
     public class TurtleModels
     {
         public TurtlePathModel TurtlePathModel { get; } = new TurtlePathModel();
+        public AnswersModel AnswersModel { get; } = new AnswersModel();
     }
 }
